495880bc829ef4728fea6d6273e0cab7790cd7fb
Kris Albro <krs5a@yahoo.com>
2017-06-22 14:14:37 -0400

    worked on styles and fnished js for robots

--- main.js
+++ main.js
@@ -10,12 +10,21 @@ application.set('/views', './views')
 application.set('view engine', 'mustache')
 
 let dataFileInfo = {
-    users: dataFile.users.slice(0, 8)
+    users: dataFile.users.slice(0,100)
 }
 
 
-application.get('/', function (req, res) {
-    res.render("index", dataFileInfo);
+application.get('/', (request, response) => {
+    response.render("index", dataFileInfo);
 });
+
+application.get('/:userId', (request, response) => {
+    var user = dataFile.users[request.params.userId - 1]
+    response.render("index2", user);
+});
+
+application.post('/index2', (request, response) =>{
+    response.redirect('index2', dataFile);
+})
 
 application.listen(4000);
